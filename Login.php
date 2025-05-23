2d0927f1d2ff3436e41e4381aa40b06b1c4e432f
Dan <hamdannr543@gmail.com>
2025-05-23 09:39:54 +0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -13,7 +13,6 @@
         <form class="Login-form">
             <Input type="text" placeholder="Username" required></Input>
             <Input type="password" placeholder="Password" required></Input>
-            <a href="Reset Pass.php">Forgot your password?</a>
             <button type="submit">LOGIN</button>
             <div class="Btext">
             Doesn't have an account? <a href="Register.php" >Register</a>
